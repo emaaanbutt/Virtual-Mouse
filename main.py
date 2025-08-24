7eeb6f2f8b9a0bb5efd84ccdfec62ff35e09fb67
Eman Butt <butt.eman391@gmail.com>
2025-08-24 19:27:15 +0500

    Added click functionality.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import math
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -24,13 +25,16 @@ with mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7) a
 
                 h, w, c = frame.shape
 
-                # Index finger landmarks
+                # Index finger and thumb landmarks
                 index_finger = hand_landmarks.landmark[8]
+                thumb = hand_landmarks.landmark[4]
 
                 # convert normalized landmark coords to camera pixels
                 ix, iy = int(index_finger.x * w), int(index_finger.y * h)
+                tx, ty = int(thumb.x * w), int(thumb.y * h)
 
                 cv2.circle(frame, (ix, iy), 10, (0, 255, 0), -1)
+                cv2.circle(frame, (tx, ty), 10, (0, 0, 255), -1)
 
                  # convert normalized landmark coords to screen pixels
                 screen_x = int(index_finger.x * screen_w)
@@ -45,6 +49,15 @@ with mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7) a
 
                 # move cursor
                 pyautogui.moveTo(smooth_x, smooth_y)
+
+                 # Distance between thumb and index finger
+                distance = math.hypot(tx - ix, ty - iy)
+
+                # Click if fingers are close
+                if distance < 40:
+                    pyautogui.click()
+                    pyautogui.sleep(0.2)  # Small delay to avoid double click
+
                
 
         cv2.imshow("Webcam", frame)
